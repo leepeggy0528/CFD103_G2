416afb7490fe38688d5a822892303e24590c1a6e
Sonya0115 <a0931821151@gmail.com>
2021-12-16 15:42:14 +0800

    加上會員連結

--- src/js/friendtoggle.js
+++ src/js/friendtoggle.js
@@ -1,6 +1,6 @@
-$(".edit").click(function (e) {
-    $(e.target).closest("div").toggleClass("openControl");
-   });
+// $(".edit").click(function (e) {
+//     $(e.target).closest("div").toggleClass("openControl");
+//    });
 
 $("#afterLogin").click(function (e) {
     $(".smallMemInfo").toggleClass("openInfo");
