04d755806ce57f931f7e23ae5f651bc8e310db54
daedae0621 <daedae0621@gmail.com>
2021-12-16 15:17:23 +0800

    增加js

--- src/js/group.js
+++ src/js/group.js
@@ -2,13 +2,47 @@ let saveActivity;
 let filterBtn;
 let filter;
 
-function switchFilter() {
-    let filter = document.querySelector('.filter');
-    console.log(filter);
-    filter.classList.toggle("show-filter");
-
+// sort-nav focus樣式
+function sortFocus(e) {
+    let checkStyle = document.querySelectorAll(".sort > a")
+    console.log(checkStyle);
+    for (let i = 0; i < checkStyle.length; i++) {
+        checkStyle[i].classList.remove("sortFocus");
+    }
+    e.target.classList.add("sortFocus");
 }
 
+
+// 開關選取器
+function switchFilter() {
+    let filter = document.querySelector('.filter');
+    filter.classList.toggle("show-filter");
+}
+
+//選擇活動focus效果
+function themeFocus(e) {
+    let ul = document.querySelector(".filter-main .theme");
+    let li = e.target;
+    // console.log(ul.children[0].classList.value);
+    if (li.innerText != "全部") {
+        ul.firstElementChild.classList.remove("filterFocus");
+        li.classList.toggle("filterFocus");
+        // while (a = 0) {
+        // for (let j = 0; j < ul.children.length; j++) {
+        //     console.log(ul.children[j].classList.value.includes("filterFocus"));
+        // }
+        // }
+
+    } if (li.innerText == "全部") {
+        for (let i = 0; i < ul.children.length; i++) {
+            ul.children[i].classList.remove("filterFocus");
+
+        }
+        ul.firstElementChild.classList.add("filterFocus");
+    }
+
+}
+// 收藏活動 
 
 function switchSaveActivity(e) {
     if (e.target.title == "收藏活動") {
@@ -19,16 +53,118 @@ function switchSaveActivity(e) {
         e.target.title = "收藏活動";
     }
 }
+
+
+// 去除多餘字元
+function sliceTitle() {
+    let groupTitle = document.querySelectorAll('.party_text .main h3 a');//活動名稱
+    let cardTitle = document.querySelectorAll('.pageGroup .card'); //卡片title屬性值
+    let screenWidth = window.screen.width; // 抓視窗大小
+
+    for (let i = 0; i < groupTitle.length; i++) {
+        if (screenWidth <= 375) {
+            if (cardTitle[i].title.length > 15) {
+                groupTitle[i].innerText = cardTitle[i].title.slice(0, 15) + "...";
+            }
+
+        } else {
+
+            if (cardTitle[i].title.length > 25) {
+                groupTitle[i].innerText = cardTitle[i].title.slice(0, 25) + "...";
+            }
+        }
+    }
+}
+
 function init() {
-    //選取器
+    // 選取器收合
     filterBtn = document.querySelector('#filterIcon');
     filterBtn.onclick = switchFilter;
 
-    //收藏
+    // 選取主題地點focus效果
+    document.querySelector(".filter-main .theme").onclick = themeFocus;
+
+    // 下拉式選單
+    let selectArea = document.querySelector("#selectArea");
+    let optionAll = document.querySelectorAll("#selectLoc>option");
+    let optionNorth = document.querySelectorAll("#selectLoc .north");
+    console.log(optionNorth);
+    selectArea.onchange = () => {
+        // alert(selectArea.value);
+        switch (selectArea.value) {
+            case "全部":
+                for (let i = 0; i < optionAll.length; i++) {
+                    optionAll[i].style.display = "block";
+                }
+                break;
+            case "北部":
+                for (let l = 0; l < optionAll.length; l++) {
+                    if (optionAll[l].classList.value.indexOf('north') == -1) {
+                        optionAll[l].style.display = "none";
+                    } else {
+                        optionAll[l].style.display = "block";
+                    }
+
+                }
+
+                break;
+            case "中部":
+                for (let l = 0; l < optionAll.length; l++) {
+                    if (optionAll[l].classList.value.indexOf('central') == -1) {
+                        optionAll[l].style.display = "none";
+                    } else {
+                        optionAll[l].style.display = "block";
+                    }
+
+                }
+                break;
+            case "南部":
+                for (let l = 0; l < optionAll.length; l++) {
+                    if (optionAll[l].classList.value.indexOf('south') == -1) {
+                        optionAll[l].style.display = "none";
+                    } else {
+                        optionAll[l].style.display = "block";
+                    }
+                }
+                break;
+            case "離島":
+                for (let l = 0; l < optionAll.length; l++) {
+                    if (optionAll[l].classList.value.indexOf('outlying') == -1) {
+                        optionAll[l].style.display = "none";
+                    } else {
+                        optionAll[l].style.display = "block";
+                    }
+                }
+                break;
+
+        }
+    }
+
+
+    // 收藏
     saveActivity = document.querySelectorAll('#saveActivity');
     for (let i = 0; i < saveActivity.length; i++) {
         saveActivity[i].onclick = switchSaveActivity;
     }
 
+    //sort-nav
+    let sortNav = document.querySelector(".sort");
+    sortNav.onclick = sortFocus;
+
+    // 取得標題字
+    let groupTitle = document.querySelectorAll('.party_text .main h3 a');
+    let card = document.querySelectorAll('.pageGroup .card');
+    for (let i = 0; i < card.length; i++) {
+        card[i].title = groupTitle[i].innerText;
+    }
+
+    // 去除多餘字元
+    sliceTitle();
 }
+
+function windowResize() {
+    sliceTitle();
+}
+
 window.addEventListener("load", init, false);
+window.addEventListener("resize", windowResize, false);
