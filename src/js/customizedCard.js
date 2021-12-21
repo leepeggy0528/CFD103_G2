06bf4404f6fa7474382ddfc545906d658f4b593a
daedae0621 <daedae0621@gmail.com>
2021-12-21 09:57:33 +0800

    no message

--- src/js/customizedCard.js
+++ src/js/customizedCard.js
@@ -167,7 +167,8 @@ function drawCanvas() {
 
     // 取得輸入文字
     let inputText = document.querySelector('.inputText');
-    let cardImage = document.getElementById('preivewCardPattern')
+    let cardImage = document.getElementById('preivewCardPattern');
+    console.log(cardImage);
     let textTop = inputText.offsetTop - cardImage.offsetTop;
     let textLeft = cardImage.offsetLeft - inputText.offsetLeft;
     ctx.fillText(inputText.innerText, 10, textTop);
