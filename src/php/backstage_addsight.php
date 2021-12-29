d57766b6aafedda47e438b19c25eeb11e5be0c83
leepeggy0528 <peggy6088@gmail.com>
2021-12-29 19:29:23 +0800

    backstage_sight新增

--- src/php/backstage_addsight.php
+++ src/php/backstage_addsight.php
@@ -1,35 +1,46 @@
 <?php 
+    require_once('./connectAccount.php');
+    $sql = "INSERT INTO `sight` VALUES (Null, :sig_name, :sig_describe,:sig_intro, :sig_phone, :sig_adress, :sig_time, :sig_web, :sig_type, :sig_loc)";
+                $products = $pdo->prepare( $sql );
+                $products -> bindValue(":sig_name", $_POST["name"]);
+                $products -> bindValue(":sig_describe", $_POST["desc"]);
+                $products -> bindValue(":sig_intro", $_POST["intro"]);
+                $products -> bindValue(":sig_phone", $_POST["tel"]);
+                $products -> bindValue(":sig_adress",$_POST["loc"].$_POST["address"]);
+                $products -> bindValue(":sig_time", $_POST["time"]);
+                $products -> bindValue(":sig_web", $_POST["web"]);
+                $products -> bindValue(":sig_type", $_POST["type"]);
+                $products -> bindValue(":sig_loc", $_POST["loc"]);
+                $products -> execute();		
+    $sig_no = $pdo->lastInsertId();	
     foreach($_FILES["upFile"]["error"] as $i => $data)
     {
-        switch ($data) {
-            case UPLOAD_ERR_OK:
-                $dir = "images";
-                if (file_exists("images")==false) {
-                    mkdir($dir);
-                }
-                $from = $_FILES["upFile"]["tmp_name"][$i];
-                $to = "$dir/".$_FILES["upFile"]["name"][$i];
-                if (copy($from,$to)) {
-                    echo "<script>alert('OK~~~');location.href='./fileUploadMany.html';</script>";
-                }else{
-                    echo "NG!!!<br>";
-                }
-                break;
-            case UPLOAD_ERR_INI_SIZE:
-                echo "檔案太大,不得超過".ini_get("upload_max_filesize")."<br>";
-                break;
-            case UPLOAD_ERR_FORM_SIZE:
-                echo "檔案太大,不得超過".$_POST['MAX_FILE_SIZE']."bytes <br>";
-                break;
-            case UPLOAD_ERR_PARTIAL:
-                echo "檔案上傳不完整，請重新上傳<br>";
-                break;
-            case UPLOAD_ERR_NO_FILE:
-                echo "未選取檔案~~<br>";
-                break;
-            default:
-                # code...
-                break;
+        if( $data == UPLOAD_ERR_OK){
+            //決定檔案名稱
+            $fileInfoArr = pathinfo($_FILES["upFile"]["name"][$i]);
+            $imageNo = uniqid();
+            $fileName = "{$imageNo}.{$fileInfoArr["extension"]}";  //312543544.gif
+            //先檢查images資料夾存不存在
+            if( file_exists("../images/sight") === false){
+                mkdir("../images/sight");
+            }
+            //將檔案copy到要放的路徑
+            $from = $_FILES["upFile"]["tmp_name"][$i];
+            $to = "../images/sight/$fileName";
+            if(copy( $from, $to)===true){
+                $sql = "INSERT INTO `sight_pt` VALUES (null, :sig_no, :spt_pt)";
+                $products = $pdo->prepare( $sql );
+                $products -> bindValue(":sig_no",$sig_no);
+                $products -> bindValue(":spt_pt", $fileName);
+                $products -> execute();		
+                echo "新增成功~";
+            }else{
+                echo "失敗~";
+            }
+
+        }else{
+            echo "錯誤代碼 : {$data} <br>";
+            echo "新增失敗<br>";
         }
     }
 ?>
